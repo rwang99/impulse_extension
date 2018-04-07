82733155300a5167cdacc493d9aa0d10e037c884
Richard Wang <rkwang@umd.edu>
2018-04-07 00:37:12 -0400

    test

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ var p = document.getElementsByClassName("place-order-button-link");
 
 $("#sc-buy-box-ptc-button").click(function(){
     var mod = document.createElement('div');
-    $('#sc-subtotal-amount-activecart').after('<div style="background-color:yellow"> New div </div>');
     mod.setAttribute('modal', '');
     document.querySelector('#sc-new-upsell').appendChild(mod);
     alert("Test: " + $("#sc-subtotal-amount-activecart").text());
