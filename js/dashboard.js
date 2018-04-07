0fc7d72080fb203d90018fbc5351c38bc1e77008
Michael <michael.weinberger09@gmail.com>
2018-04-07 19:06:43 -0400

    Sign In and Logout Completed

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,10 +1,39 @@
-$( "#signin-button" ).click(function() {
-    const email = $('#name').val();
-    const password = $('#password').val();
-    const data = { email, password };
+$(document).ready(function(){
 
-    $.post( "http://localhost:3000/sign-in-extension", data, function( user ) {
-        // Now the global userId has a value. The user is logged in and can see their data.
-        localStorage.setItem('user', JSON.stringify(user));
+    if (localStorage.getItem('user') == "null"){
+        $(".not-signed-in").show();
+        $(".signed-in").hide();
+    }
+    else{
+        $("#welcome-text").html("Welcome, " + JSON.parse(localStorage.getItem('user')).user.name);
+        $(".not-signed-in").hide();
+        $(".signed-in").show();
+    }
+
+    $( "#signin-button" ).click(function() {
+        const email = $('#name').val();
+        const password = $('#password').val();
+        const data = { email, password };
+
+        $.post( "http://localhost:3000/sign-in-extension", data, function( user ) {
+            // Now the global userId has a value. The user is logged in and can see their data.
+            localStorage.setItem('user', JSON.stringify(user));
+
+            $(".not-signed-in").hide();
+            $(".signed-in").show();
+
+            $("#welcome-text").html("Welcome, " + user.user.name);
+
+        });
     });
-});
+
+    $ ( "#logout-button").click(function(){
+
+        localStorage.setItem('user', null);
+
+        $(".not-signed-in").show();
+        $(".signed-in").hide();
+
+    })
+
+})
