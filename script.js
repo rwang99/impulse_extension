3589b3d06e488cdc4621fb363d591026e3bdcc8e
Michael <michael.weinberger09@gmail.com>
2018-04-07 14:49:40 -0400

    Added Comments

--- script.js
+++ script.js
@@ -1,39 +1,79 @@
+let poppedUp = false;
+
 // When the user presses proceed to checkout
+
 $("#sc-buy-box-ptc-button").click(function(e){
 
-    let numCharRequired = 100;
+    // If the user has not put in their justification yet
+    if (poppedUp == false){
 
-    e.preventDefault(); // Pause the request
+        e.preventDefault(); // Pause the request
 
-    let modalHTML = '<div class="modal-impulse">'+
-                        '<div class = "container-fluid modal-impulse-content">'+
-                            '<div class = "modal-impulse-header row">' +
-                                '<h1 class = "col-md-12 text-center">Impulse</h1>' +
-                            '</div>' +
-                            '<div class = "modal-impulse-main row">' +
-                                '<h2 class = "text-center modal-light col-md-12" id = "impulse-main-instruct">Why Are You Buying This?</h2>' +
-                                '<br>' +
-                                `<h5 class = "char-count modal-light col-md-12 text-center">${numCharRequired} characters left until you can purchase this item.</h5>` +
-                                '<br>' +
-                                '<form class = "col-md-12">' +
-                                    '<div class="form-group col-md-6 offset-md-3">' +
-                                        '<textarea class="form-control impulse-reason-box" rows="5"></textarea>' +
+        let numCharRequired = 50;
+        let modalHTML = '<div class = "modal-impulse-wrapper">' +
+                            '<div class="modal-impulse">'+
+                                '<div class = "container-fluid modal-impulse-content">'+
+                                    '<div class = "modal-impulse-header row">' +
+                                        '<h1 class = "col-md-12 text-center">Impulse</h1>' +
                                     '</div>' +
-                                    '<br>' +
-                                    '<div class = "text-center">' +
-                                        '<button type="submit" class="btn btn-primary">Buy Anyway <i class="fas fa-credit-card"></i></button> ' +
-                                        '<button class="btn btn-primary">This Is An Impulse Purchase <i class="fas fa-ban"></i></button>' +
-                                    '</div>'+
-                                '</form>' +
+                                    '<div class = "modal-impulse-main row">' +
+                                        '<h2 class = "text-center modal-light col-md-12" id = "impulse-main-instruct">Why Are You Buying This?</h2>' +
+                                        '<br>' +
+                                        `<h5 class = "char-count modal-light col-md-12 text-center">${numCharRequired} characters left until you can purchase this item.</h5>` +
+                                        '<br>' +
+                                        '<form class = "col-md-12">' +
+                                            '<div class="form-group col-md-6 offset-md-3">' +
+                                                '<textarea class="form-control impulse-reason-box" rows="5"></textarea>' +
+                                            '</div>' +
+                                            '<br>' +
+                                            '<div class = "text-center">' +
+                                                '<button class="btn btn-primary" id = "impulse-modal-buy">Buy Anyway</button> ' +
+                                                '<button class="btn btn-primary" id = "impulse-modal-cancel">This Is An Impulse Purchase</button>' +
+                                            '</div>'+
+                                        '</form>' +
+                                    '</div>' +
+                                '</div>' +
                             '</div>' +
-                        '</div>' +
-                    '</div>';
+                        '</div>';
 
-    $('body').append(modalHTML);
+        // Inject the modal html into the page
+        $('body').append(modalHTML);
 
-    $('.impulse-reason-box').on('keyup', function(event) {
-       let numChar = numCharRequired - $(this).val().length;
-      $('.char-count').html(numChar + " characters left until you can purchase this item.");
-    });
+        // Disbale the buy now button until the use puts in the correct number of characters
+        if (numCharRequired > 0){
+            $('#impulse-modal-buy').attr('disabled', true);
+        }
+
+        // Function that tracks how many more characters the user has to put ini
+        $('.impulse-reason-box').on('keyup', function(event) {
+            let numCharLeft = numCharRequired - $(this).val().length;
+            if (numCharLeft < 0){
+                numCharLeft = 0;
+            }
+            $('.char-count').html(numCharLeft + " characters left until you can purchase this item.");
+            if (numCharLeft == 0){
+                $('#impulse-modal-buy').attr('disabled', false);
+            }
+            else{
+                $('#impulse-modal-buy').attr('disabled', true);
+            }
+        });
+
+        // If the user clicks the buy now button then they have inputted the min number of chars
+        $("#impulse-modal-buy").click(function(){
+            poppedUp = true; // Alter the boolean
+            $('#sc-buy-box-ptc-button').trigger('click'); // Allow them to continue with their checkout
+        })
+
+        // If the user cancels the purchase
+        $("#impulse-modal-cancel").click(function(){
+            $(".modal-impulse-wrapper").html(""); // Blank the modal
+        })
+
+    }
+
+    else { // If the user has already inputted their justification, don't bring the modal again
+        $(".modal-impulse-wrapper").html("");
+    }
 
 });
