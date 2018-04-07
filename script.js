395b39915b8b53958e26496d6b5a3f21ffe4fc26
Michael <michael.weinberger09@gmail.com>
2018-04-07 00:36:05 -0400

    Modal Working

--- script.js
+++ script.js
@@ -2,12 +2,35 @@
 //Regex to find the place order button
 var p = document.getElementsByClassName("place-order-button-link");
 
-
+/*
 $("#sc-buy-box-ptc-button").click(function(){
     var mod = document.createElement('div');
     $('#sc-subtotal-amount-activecart').after('<div style="background-color:yellow"> New div </div>');
     mod.setAttribute('modal', '');
     document.querySelector('#sc-new-upsell').appendChild(mod);
     alert("Test: " + $("#sc-subtotal-amount-activecart").text());
+
+});
+
+*/
+
+$("#sc-buy-box-ptc-button").click(function(){
+    event.preventDefault();
+
+    $('body').append('<div class="modal"><div class = "modal-content"> HELO' + $("#sc-subtotal-amount-activecart").text() + '</div></div>');
+
+    event.submit();
+
+});
+
+$("#sc-buy-box-ptc-button").submit(function(e){
+
+    e.preventDefault();
+
+    $('body').append('<div class="modal"><div class = "modal-content"> HELO' + $("#sc-subtotal-amount-activecart").text() + '</div></div>');
+
+    setInterval(function(){
+        e.submit();
+    },1000);
 
 });
