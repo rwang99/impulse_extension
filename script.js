07b0563d0c7bf12bdeceac906177253df53b66e2
Michael <michael.weinberger09@gmail.com>
2018-04-09 19:36:56 -0400

    Redesigned Popup, fixed element id and class names, fixed logic

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let poppedUp = false;
+let authorizedPurchase = false;
 let itemPriceAccountedFor = false;
 
 let appId = 'impulse-gmdpe';
 let stitchClientPromise = stitch.StitchClientFactory.create(appId);
 
-let modalHTML = '<div class = "modal-impulse-wrapper">' +
+let modalHTML = '<div class = "impulse-modal-wrapper">' +
 
                 '</div>';
 
@@ -16,7 +16,9 @@ $('body').append(modalHTML);
 // When the user presses proceed to checkout button stop action
 $("#sc-buy-box-ptc-button").click(function(e){
 
-    e.preventDefault(); // Pause the request
+    if (authorizedPurchase == false){ // If the user has not put in their justification yet
+        e.preventDefault(); // Pause the request
+    }
 
     $.get( "https://freegeoip.net/json/", function( data ) {
         userIpAddress = data.ip;
@@ -27,28 +29,30 @@ $("#sc-buy-box-ptc-button").click(function(e){
 
             if (userId !== ""){ // If they are logged in (if their IP is in the DB)
 
+                console.log(userId);
+
                 // If the user has not put in their justification yet
-                if (poppedUp == false){
+                if (authorizedPurchase == false){
 
                     let numCharRequired = 50;
-                    let loggedInModal = '<div class="modal-impulse">'+
-                        '<div class = "container-fluid modal-impulse-content">'+
-                        '<div class = "modal-impulse-header row">' +
+                    let loggedInModal = '<div class="impulse-modal">'+
+                        '<div class = "container-fluid impulse-modal-content">'+
+                        '<div class = "impulse-modal-header row">' +
                         '<h1 class = "col-md-12 text-center">Impulse</h1>' +
                         '</div>' +
-                        '<div class = "modal-impulse-main row">' +
-                        '<h2 class = "text-center modal-light col-md-12" id = "impulse-main-instruct">Why Are You Buying This?</h2>' +
+                        '<div class = "impulse-modal-main row">' +
+                        '<h2 class = "text-center impulse-modal-light col-md-12" id = "impulse-main-instruct">Why Are You Buying This?</h2>' +
                         '<br>' +
-                        `<h5 class = "char-count modal-light col-md-12 text-center">${numCharRequired} characters left until you can purchase this item.</h5>` +
+                        `<h5 class = "impulse-modal-char-count impulse-modal-light col-md-12 text-center">${numCharRequired} characters left until you can purchase this item.</h5>` +
                         '<br>' +
                         '<form class = "col-md-12">' +
                         '<div class="form-group col-md-6 offset-md-3">' +
-                        '<textarea class="form-control impulse-reason-box" rows="5"></textarea>' +
+                        '<textarea class="form-control impulse-modal-reason-box" rows="5"></textarea>' +
                         '</div>' +
                         '<br>' +
                         '<div class = "text-center">' +
-                        '<button class="btn btn-primary" id = "impulse-modal-buy">Buy Anyway</button> ' +
-                        '<button class="btn btn-primary" id = "impulse-modal-cancel">This Is An Impulse Purchase</button>' +
+                        '<button class="btn btn-primary" id = "impulse-modal-buy-button">Buy Anyway</button> ' +
+                        '<button class="btn btn-primary" id = "impulse-modal-cancel-button">This Is An Impulse Purchase</button>' +
                         '</div>'+
                         '</form>' +
                         '</div>' +
@@ -56,32 +60,32 @@ $("#sc-buy-box-ptc-button").click(function(e){
                         '</div>';
 
                     // Inject the modal html into the page
-                    $('.modal-impulse-wrapper').html(loggedInModal);
+                    $('.impulse-modal-wrapper').html(loggedInModal);
 
                     // Disbale the buy now button until the use puts in the correct number of characters
                     if (numCharRequired > 0){
-                        $('#impulse-modal-buy').attr('disabled', true);
+                        $('#impulse-modal-buy-button').attr('disabled', true);
                     }
 
                     // Function that tracks how many more characters the user has to put ini
-                    $('.impulse-reason-box').on('keyup', function(event) {
+                    $('.impulse-modal-reason-box').on('keyup', function(event) {
                         let numCharLeft = numCharRequired - $(this).val().length;
                         if (numCharLeft < 0){
                             numCharLeft = 0;
                         }
-                        $('.char-count').html(numCharLeft + " characters left until you can purchase this item.");
+                        $('.impulse-modal-char-count').html(numCharLeft + " characters left until you can purchase this item.");
                         if (numCharLeft == 0){
-                            $('#impulse-modal-buy').attr('disabled', false);
+                            $('#impulse-modal-buy-button').attr('disabled', false);
                         }
                         else{
-                            $('#impulse-modal-buy').attr('disabled', true);
+                            $('#impulse-modal-buy-button').attr('disabled', true);
                         }
                     });
 
                     // If the user clicks the buy now button then they have inputted the min number of chars
-                    $("#impulse-modal-buy").click(function(){
+                    $("#impulse-modal-buy-button").click(function(){
 
-                        poppedUp = true; // Alter the boolean
+                        authorizedPurchase = true; // Alter the boolean
 
                         let purchasedData = {
 
@@ -89,7 +93,7 @@ $("#sc-buy-box-ptc-button").click(function(e){
                             itemPrice: $('.sc-product-price')[0].innerHTML.replace('$',''),
                             purchaseDate: (new Date).getTime(),
                             wasPurchased: true,
-                            decisionReason: $(".impulse-reason-box").val(),
+                            decisionReason: $(".impulse-modal-reason-box").val(),
 
                         };
 
@@ -114,12 +118,12 @@ $("#sc-buy-box-ptc-button").click(function(e){
                             });
                         });
 
-                        $(".modal-impulse-wrapper").html(""); // Blank the modal
+                        $(".impulse-modal-wrapper").html(""); // Blank the modal
                         $('#sc-buy-box-ptc-button').trigger('click'); // Allow them to continue with their checkout
                     })
 
                     // If the user cancels the purchase
-                    $("#impulse-modal-cancel").click(function(){
+                    $("#impulse-modal-cancel-button").click(function(){
                         if (!itemPriceAccountedFor){ // If we have not accounted for the savings, account for them
 
                             let unpurchasedData = {
@@ -154,40 +158,38 @@ $("#sc-buy-box-ptc-button").click(function(e){
                             });
 
                         }
-                        $(".modal-impulse-wrapper").html(""); // Blank the modal
+                        $(".impulse-modal-wrapper").html(""); // Blank the modal
                         itemPriceAccountedFor = true;
                     })
 
                 }
 
                 else { // If the user has already inputted their justification, don't bring the modal again
-                    $(".modal-impulse-wrapper").html("");
+                    $(".impulse-modal-wrapper").html("");
                 }
 
-            } else { // If the user is not logged
+            } else { // If the user is not logged in
 
-                e.preventDefault(); // Pause the request
-
-                let notLoggedInModal = '<div class="modal-impulse">'+
-                    '<div class = "container-fluid modal-impulse-content">'+
-                    '<div class = "modal-impulse-header row">' +
+                let notLoggedInModal = '<div class="impulse-modal">'+
+                    '<div class = "container-fluid impulse-modal-content">'+
+                    '<div class = "impulse-modal-header row">' +
                     '<h1 class = "col-md-12 text-center">Impulse</h1>' +
                     '</div>' +
                     '<br>' +
-                    '<div class = "modal-impulse-main row">' +
-                    '<h2 class = "text-center modal-light col-md-12">Please Log In Using the Extension.</h2>' +
+                    '<div class = "impulse-modal-main row">' +
+                    '<h2 class = "text-center impulse-modal-light col-md-12">Please Log In Using the Extension.</h2>' +
                     '</div>' +
                     '<div class = "row">' +
-                    '<div class = "col-md-12 text-center"><button class="btn btn-primary" id = "impulse-modal-close">Close</button></div>' +
+                    '<div class = "col-md-12 text-center"><button class="btn btn-primary" id = "impulse-modal-close-button">Close</button></div>' +
                     '</div>' +
                     '</div>' +
                     '</div>';
 
                 // Inject the modal html into the page
-                $('.modal-impulse-wrapper').html(notLoggedInModal);
+                $('.impulse-modal-wrapper').html(notLoggedInModal);
 
-                $('#impulse-modal-close').click(function(){
-                    $('.modal-impulse-wrapper').html("");
+                $('#impulse-modal-close-button').click(function(){
+                    $('.impulse-modal-wrapper').html("");
                 })
 
             }
